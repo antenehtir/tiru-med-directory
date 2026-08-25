c59220d38e9f5304d792d68bb0923996d5099670
antenehtir <161809149+antenehtir@users.noreply.github.com>
2026-08-25 19:57:40 +0300

    Polish facility detail information architecture

--- src/components/facility-detail/FacilityDetailPage.tsx
+++ src/components/facility-detail/FacilityDetailPage.tsx
@@ -7,6 +7,7 @@ import { FacilityDoctorsSection } from "./FacilityDoctorsSection";
 import { FacilityHoursSection } from "./FacilityHoursSection";
 import { FacilityInformationSection } from "./FacilityInformationSection";
 import { FacilityServicesSection } from "./FacilityServicesSection";
+import { FacilityTrustSection } from "./FacilityTrustSection";
 import { SimilarFacilitiesSection } from "./SimilarFacilitiesSection";
 
 type FacilityDetailPageProps = { facility?: Facility; similarFacilities?: Facility[] };
@@ -16,26 +17,30 @@ export function FacilityDetailPage({ facility, similarFacilities }: FacilityDeta
   const selectedSimilarFacilities = similarFacilities ?? [];
 
   return (
-    <PageContainer className="py-6 sm:py-10 lg:py-14">
-      <div className="grid gap-6 sm:gap-8">
-        <div className="grid gap-4 lg:grid-cols-[1fr_22rem] lg:items-start lg:gap-6">
+    <PageContainer className="py-4 sm:py-8 lg:py-12">
+      <div className="grid gap-5 sm:gap-7">
+        <div className="grid gap-4 lg:grid-cols-[minmax(0,1fr)_22rem] lg:items-start lg:gap-6">
           <FacilityDetailHeader facility={facility} />
 
           <div className="lg:sticky lg:top-24">
-            <div className="hidden lg:block">
-              <FacilityActionPanel facility={facility} />
-            </div>
-            <div className="lg:hidden">
-              <FacilityActionPanel facility={facility} />
-            </div>
+            <FacilityActionPanel facility={facility} />
           </div>
         </div>
 
-        <div className="grid gap-6 sm:gap-8">
-          <FacilityServicesSection facility={facility} />
-          <FacilityDoctorsSection facility={facility} />
-          <FacilityHoursSection facility={facility} />
-          <FacilityInformationSection facility={facility} />
+        <div className="grid gap-5 sm:gap-7">
+          <section aria-labelledby="care-heading" className="grid gap-5 sm:gap-7">
+            <div id="care-heading" className="sr-only">Care and clinical information</div>
+            <FacilityServicesSection facility={facility} />
+            <FacilityDoctorsSection facility={facility} />
+          </section>
+
+          <section aria-labelledby="visit-heading" className="grid gap-5 sm:gap-7">
+            <div id="visit-heading" className="sr-only">Plan your visit</div>
+            <FacilityHoursSection facility={facility} />
+            <FacilityInformationSection facility={facility} />
+          </section>
+
+          <FacilityTrustSection facility={facility} />
         </div>
 
         <CorrectionCta facilitySlug={facility.slug} />
